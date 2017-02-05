67549e3379186a13d435620c169a7b6a852cfcdc
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-02-05 22:49:11 +0100

    update

--- build/src/icons/icons.config.js
+++ build/src/icons/icons.config.js
@@ -2,4 +2,9 @@ const _ = require('lodash')
 
 const icons = require('./ita/font/selection.json').icons
 
-module.exports = { context: { icons: _.map(icons, (v) => v.properties.name) }}
+module.exports = {
+  order: 60,
+  context: {
+    icons: _.map(icons, (v) => v.properties.name)
+  }
+}
