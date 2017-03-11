4152c51b321dd0d57badabe35073520f1130e76b
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-03-11 18:32:04 +0100

    fix dropdown arrow with popperjs

--- src/modules/dropdown/index.js
+++ src/modules/dropdown/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import Popper from 'popper.js'
 import {
   Menu,
   Menutrigger
@@ -24,6 +25,29 @@ $.fn['menu-trigger'] = function() {
 
 $(document).bind('enhance', function(e) {
   $('[data-menu-trigger]', e.target)['menu-trigger']()
+})
+
+////////////////////////////////////
+
+$('[data-menu-trigger]').each((i, el) => {
+  const $target = $('#' + $(el).attr('data-menu-trigger'))
+
+  if (el && $target.length > 0) {
+    new Popper(el, $target, {
+          placement: 'bottom',
+          modifiers: {
+              arrow: {
+                element: '.Dropdown-arrow'
+              },
+              flip: {
+                  behavior: ['left', 'right']
+              },
+              preventOverflow: {
+                  boundariesElement: 'viewport'
+              },
+          },
+    })
+  }
 })
 
 export default {
