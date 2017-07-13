9e6b769314d26ef3296d455bdd718faecf067d27
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-07-13 16:57:18 +0200

    aggiornato bottone spid

--- src/modules/spid/spid.config.js
+++ src/modules/spid/spid.config.js
@@ -6,6 +6,8 @@ let IDP = [
   { img: 'posteid', idp: 'Poste ID' },
   { img: 'sielteid', idp: 'Sielte ID' },
   { img: 'arubaid', idp: 'Aruba ID' },
+  { img: 'namirialid', idp: 'Namirial ID' },
+  { img: 'spiditalia', idp: 'SPIDItalia Register.it' },
 ]
 
 module.exports = { context: {IDP} }
