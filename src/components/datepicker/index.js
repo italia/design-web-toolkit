b2563122679204859f3e06440e7957847e8cd040
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-02-08 15:26:37 +0100

    fix: export datepicker

--- src/components/datepicker/index.js
+++ src/components/datepicker/index.js
@@ -1,24 +1,53 @@
 import $ from 'jquery'
 
-$('.js-Datepicker').each((i, el) => {
-  $(el).attr('type', 'text')
+const datepickerInit = cb => require(['moment', 'pikaday'], cb)
 
-  require(['moment', 'pikaday'], (Moment, Pikaday) => {
-    new Pikaday({
-      field: $(el)[0],
-      trigger: $(`[aria-controls='${el.id}']`)[0],
-      format: 'DD/MM/YYYY',
-      i18n: {
-        previousMonth: 'Mese precedente',
-        nextMonth: 'Mese successivo',
-        months: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-        weekdays: ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'],
-        weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab']
-      },
-      // onSelect: () => {
-      //   console.log(this.getMoment().format('Do MMMM YYYY'))
-      // }
+const dateInputs = $('.js-Datepicker')
+
+if (dateInputs.length > 0) {
+  datepickerInit((Moment, Pikaday) => {
+    dateInputs.each((i, el) => {
+      $(el).attr('type', 'text')
+      new Pikaday({
+        field: $(el)[0],
+        trigger: $(`[aria-controls='${el.id}']`)[0],
+        format: 'DD/MM/YYYY',
+        i18n: {
+          previousMonth: 'Mese precedente',
+          nextMonth: 'Mese successivo',
+          months: [
+            'Gennaio',
+            'Febbraio',
+            'Marzo',
+            'Aprile',
+            'Maggio',
+            'Giugno',
+            'Luglio',
+            'Agosto',
+            'Settembre',
+            'Ottobre',
+            'Novembre',
+            'Dicembre'
+          ],
+          weekdays: [
+            'Domenica',
+            'Lunedì',
+            'Martedì',
+            'Mercoledì',
+            'Giovedì',
+            'Venerdì',
+            'Sabato'
+          ],
+          weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab']
+        }
+        // onSelect: () => {
+        //   console.log(this.getMoment().format('Do MMMM YYYY'))
+        // }
+      })
     })
   })
+}
 
-})
+export default {
+  datepickerInit
+}
