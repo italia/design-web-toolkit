bb5bef63fe37b23167f298e5b94734f9ec376d7a
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-02-12 16:11:39 +0100

    fix: focus and promise polyfill on IE

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Config = require('./config')
 
+/* Promise polyfill for IE10 */
+import Promise from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = Promise
+}
+
 /* eslint-disable */
 if (window.__PUBLIC_PATH__) {
   __webpack_public_path__ = window.__PUBLIC_PATH__
@@ -26,13 +33,13 @@ function requireAll(requireContext) {
   requireContext.keys().forEach((filename) => {
     const dirname = filename.replace(/\\/g, '/').replace(/\/[^\/]*$/, '')
 
-    const exclude = -1 !== _findIndex(Config.excludes, function(v) {
+    const exclude = -1 !== _findIndex(Config.excludes, function (v) {
       return dirname.match(new RegExp(v)) !== null
     })
 
     const include = !exclude &&
       (Config.includes.length === 0 ||
-        (-1 !== _findIndex(Config.includes, function(v) {
+        (-1 !== _findIndex(Config.includes, function (v) {
           return dirname.match(new RegExp(v)) !== null
         })))
 
